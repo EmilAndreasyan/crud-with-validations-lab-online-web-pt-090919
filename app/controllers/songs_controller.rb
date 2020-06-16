75e92ad09c424e6fe1060670c71c42cd74289fdb
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-16 07:50:02 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,51 +1,49 @@
 class SongsController < ApplicationController
-    before_action :set_song!, only: [:show, :edit, :update, :destroy]
+  def index
+    @songs = Song.all
+  end
 
-    def new
-        @song = Song.new
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to @song
+    else
+      render :new
     end
+  end
 
-    def create
-        @song = Song.new(song_params)
-        if @song.save
-            redirect_to song_path(@song)
-        else
-            render "new"
-        end
+  def update
+    @song = Song.find(params[:id])
+    if @song.update(song_params)
+      redirect_to @song
+    else
+      render :edit
     end
+  end
 
-    def index
-        @songs = Song.all
-    end
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+    redirect_to songs_url
+  end
 
-    def show
-    end
+  private
 
-    def edit
-    end
-
-    def update
-        if @song.update(song_params)
-            redirect_to song_path(@song)
-        else
-            render "edit"
-        end
-    end
-
-    def destroy
-        @song.destroy
-        redirect_to songs_path
-    end
-
-    private
-
-    def set_song!
-        @song = Song.find(params[:id])
-    end
-
-    def song_params
-        params.require(:song).permit(
-            :title, :released, :release_year, :artist_name, :genre
-            )
-    end
+  def song_params
+    params.require(:song).permit(
+      :title, :release_year, :released, :genre, :artist_name
+    )
+  end
 end
