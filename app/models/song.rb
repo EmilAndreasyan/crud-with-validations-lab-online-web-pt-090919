31f5c883bed1358296a215422dea7fa9154de7e3
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-16 07:56:22 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,21 +1,20 @@
 class Song < ActiveRecord::Base
-    validates :title, presence: true
-    validates :artist_name, presence: true
-    validate :no_repeated_title
-    validates :release_year, presence: true, 
-        if: :released?
-    validate :date_not_possible
+  validates :title, presence: true
+  validates :title, uniqueness: {
+    scope: %i[release_year artist_name],
+    message: 'cannot be repeated by the same artist in the same year'
+  }
+  validates :released, inclusion: { in: [true, false] }
+  validates :artist_name, presence: true
 
-    def no_repeated_title
-        if Song.any? {|s| s.title == title && s.artist_name == artist_name && s.release_year == release_year}
-            errors.add(:title, "can't add the same song twice")
-        end
-    end
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: {
+      less_than_or_equal_to: Date.today.year
+    }
+  end
 
-    def date_not_possible
-        if release_year.present? && release_year > Date.today.year
-            errors.add(:release_year, "release year can't be in the future")
-        end
-    end
-
+  def released?
+    released
+  end
 end
